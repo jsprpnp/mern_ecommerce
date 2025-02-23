6d74984fe447b69a615c94a4785d5dc94e7c9748
Jasper Panopio <jasperpanopio@Jaspers-MacBook-Pro.local>
2025-02-24 01:32:18 +0800

    add navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,55 @@
+import { Button, Container, Flex, HStack, Text} from "@chakra-ui/react";
+import {useColorMode,} from "./ui/color-mode";
+
+import { Link } from "react-router-dom";
 import React from 'react'
 
+//icons
+import { CiSquarePlus } from "react-icons/ci";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
+
+
+
 const navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <div>
-      
-    </div>
+   <Container maxW={"1140px"} px={4}>
+			<Flex
+				h={16}
+				alignItems={"center"}
+				justifyContent={"space-between"}
+				flexDir={{
+					base: "column",
+					sm: "row",
+				}}
+			>
+				<Text
+					fontSize={{ base: "22", sm: "28" }}
+					fontWeight={"bold"}
+					textTransform={"uppercase"}
+					textAlign={"center"}
+					bgGradient={"linear-gradient({colors.cyan.400}, {colors.blue.500})"}
+					bgClip={"text"}
+				>
+					<Link to={"/"}>Product Store 🛒</Link>
+				</Text>
+
+            <HStack spacing={2} alignItems={"center"}>
+
+            <Link to={"/create"}>
+                <Button>
+                   <CiSquarePlus fontSize={20}/>
+                </Button>
+              </Link>
+
+              <Button onClick={toggleColorMode}>
+              {colorMode === "light" ? <IoMoon /> : <LuSun size='20' />}
+              </Button>
+            
+            </HStack>
+        </Flex>
+    </Container>
   )
 }
 
