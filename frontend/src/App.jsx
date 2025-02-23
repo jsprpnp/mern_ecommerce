74b0c315e6f1d1570982840435ef578e703340bb
Jasper Panopio <jasperpanopio@Jaspers-MacBook-Pro.local>
2025-02-23 23:01:14 +0800

    add react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
-import { Button } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
 
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
 
 function App() {
   
   return (
-    <>
-    <Button>
-      Hello
-    </Button>
-    </>
+    <Box minH={"100vh"}>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='create' element={<CreatePage />} />
+      </Routes>
+  
+    </Box>
   );
 }
 
